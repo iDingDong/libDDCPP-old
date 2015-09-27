c6c3c30e99be70277d911c433872e4c0a1ecee85
iDingDong <idingdong@mogara.org>
2015-09-27 14:22:30 +0800

    Add DD::RedBlackTreeNode

--- src/standard/bits/DD_BinaryTreeNode.hpp
+++ src/standard/bits/DD_BinaryTreeNode.hpp
@@ -12,6 +12,7 @@ _DD_BEGIN
 struct EmptyBinaryTreeNode {
 	DD_ALIAS(ThisType, EmptyBinaryTreeNode);
 
+
 	ThisType* parent;
 	ThisType* left;
 	ThisType* right;
@@ -22,8 +23,45 @@ struct EmptyBinaryTreeNode {
 
 
 template <typename _ValueT>
-struct BinaryTreeNode : EmptyBinaryTreeNode, ValueTypeNested<_ValueT> {
+struct BinaryTreeNode : EmptyBinaryTreeNode {
 	DD_ALIAS(ThisType, BinaryTree<_ValueT>);
+	DD_VALUE_TYPE_NESTED(_ValueT)
+
+
+	ValueType value;
+
+
+};
+
+
+
+struct EmptyRedBlackTreeNode : EmptyBinaryTreeNode {
+	DD_ALIAS(ThisType, EmptyRedBlackTreeNode);
+
+
+#	if __cplusplus >= 201103L
+	enum Color : CheckType {
+		red = true;
+		black = false;
+	};
+#	else
+	typedef CheckType Color;
+	static Color const red = true;
+	static Color const black = false;
+#	endif
+
+
+	Color color;
+
+
+};
+
+
+
+template <typename _ValueT>
+struct RedBlackTreeNode : EmptyRedBlackTreeNode {
+	DD_ALIAS(ThisType, RedBlackTreeNode<_ValueT>);
+	DD_VALUE_TYPE_NESTED(_ValueT)
 
 
 	ValueType value;
