3ce7a44168ff1dcf6329bcce2505d0c5fda2fee0
iDingDong <idingdong@mogara.org>
2015-10-05 12:22:47 +0800

    Finished DD::Pool

--- src/standard/bits/DD_Pool.hpp
+++ src/standard/bits/DD_Pool.hpp
@@ -125,14 +125,14 @@ struct Pool_ {
 
 
 	public:
-	ProcessType clear() {
+	ProcessType clear() DD_NOEXCEPT {
 		for (LengthType current_ = 0; current_ < maximum_units; ++ current_) {
 			clear_(current_);
 		}
 	}
 
 	private:
-	ProcessType clear_(LengthType index_) {
+	ProcessType clear_(LengthType index_) DD_NOEXCEPT {
 		for (PointerType temp_ = m_free_lists_[index_]; temp_; m_free_lists_[index_] = temp_) {
 			temp_ = get_next_(temp_);
 			BasicAllocatorType::deallocate(m_free_lists_[index_], get_size_(index_));
@@ -142,7 +142,11 @@ struct Pool_ {
 
 	public:
 	PointerType allocate(SizeType size_) DD_NOEXCEPT_AS(fetch_(get_index_(size_))) {
-		return fetch_(get_index_(size_));
+		if (size_ <= maximum_size) {
+			return fetch_(get_index_(size_));
+		} else {
+			return BasicAllocatorType::allocate(size_);
+		}
 	}
 
 
@@ -153,7 +157,220 @@ struct Pool_ {
 
 	public:
 	ProcessType deallocate(PointerType pointer_, SizeType size_) DD_NOEXCEPT {
-		recycle_(pointer_, get_index_(size_));
+		if (size_ <= maximum_size) {
+			recycle_(pointer_, get_index_(size_));
+		} else {
+			BasicAllocatorType::deallocate(pointer_, size_);
+		}
+	}
+
+
+	public:
+#	if __cplusplus >= 201103L
+	template <typename ValueT__, typename... ArgumentsT__>
+	static ProcessType construct(ValueT__* pointer_, ArgumentsT__&&... arguments___) noexcept(
+		noexcept(BasicAllocatorType::construct(pointer_, forward<ArgumentsT__>(arguments___)...))
+	) {
+		BasicAllocatorType::construct(pointer_, forward<ArgumentsT__>(arguments___)...);
+	}
+#	else
+	template <typename ValueT__>
+	static ProcessType construct(ValueT__* pointer_, ValueT__ const& value___) {
+		BasicAllocatorType::construct(pointer_, value__);
+	}
+#	endif
+
+
+	public:
+	template <typename ValueT__>
+	static ProcessType destruct(ValueT__* pointer_) DD_NOEXCEPT {
+		BasicAllocatorType::destruct(pointer_);
+	}
+
+
+	public:
+	template <typename ValueT__>
+	static ProcessType destruct(ValueT__* begin_, ValueT__* end_) DD_NOEXCEPT {
+		BasicAllocatorType::destruct(begin_, end_);
+	}
+
+
+};
+
+
+
+template <typename AllocatorT_>
+struct Pool_<AllocatorT_, true> {
+	public:
+	DD_ALIAS(ThisType, Pool_<AllocatorT_ DD_COMMA true>);
+	DD_ALIAS(AllocatorType, AllocatorT_);
+	DD_ALIAS(ValueType, void);
+
+	public:
+	DD_ALIAS(PointerType, void*);
+	DD_ALIAS(BasicAllocatorType, typename AllocatorType::Basic);
+
+	public:
+	DD_ALIAS(SizeType, ::DD::SizeType);
+	DD_ALIAS(LengthType, ::DD::LengthType);
+	static DD_CONSTANT SizeType unit = sizeof(void*);
+	static DD_CONSTANT LengthType maximum_units = DDCPP_POOL_MAXIMUM_MEMORY_UNITS;
+	static DD_CONSTANT SizeType maximum_size = unit * maximum_units;
+
+
+	public:
+	DD_ALIAS(FreeListsType_, Array<void* DD_COMMA maximum_units>);
+
+
+	private:
+	FreeListsType_ m_free_lists_ DD_IN_CLASS_INITIALIZE({});
+	AllocatorType m_allocator_ DD_IN_CLASS_INITIALIZE(AllocatorType());
+
+
+	public:
+#	if __cplusplus >= 201103L
+	Pool_() = default;
+#	else
+	Pool_() throw() : m_free_lists_{{}}, m_allocator_() {
+	}
+#	endif
+
+	public:
+	DD_DELETE_COPY_CONSTRUCTOR(Pool_);
+
+	public:
+	DD_DELETE_MOVE_CONSTRUCTOR(Pool_);
+
+
+	public:
+	~Pool_() DD_NOEXCEPT {
+		clear();
+	}
+
+
+	private:
+	static SizeType DD_CONSTEXPR get_size_(LengthType index_) DD_NOEXCEPT {
+		return unit * (index_ + 1);
+	}
+
+
+	private:
+	static LengthType DD_CONSTEXPR get_index_(SizeType size_) DD_NOEXCEPT {
+		return size_ ? size_ - 1 / unit : 0;
+	}
+
+
+	private:
+	static PointerType*& get_next_(PointerType node_) DD_NOEXCEPT {
+		return reinterpret_cast<PointerType*>(node_);
+	}
+
+
+	private:
+	PointerType fetch_(LengthType index_) DD_NOEXCEPT_AS(
+		static_cast<PointerType>(m_allocator_.allocate(get_size_(index_)))
+	) {
+		PointerType temp_;
+		if (m_free_lists_[index_]) {
+			temp_ = m_free_lists_[index_];
+			m_free_lists_[index_] = get_next_(temp_);
+		} else {
+			temp_ = m_allocator_.allocate(get_size_(index_));
+		}
+		return temp_;
+	}
+
+
+	private:
+	ProcessType recycle_(PointerType pointer_, LengthType index_) DD_NOEXCEPT {
+		get_next_(pointer_) = m_free_lists_[index_];
+		m_free_lists_[index_] = pointer_;
+	}
+
+
+	public:
+	ProcessType breed(SizeType size_) DD_NOEXCEPT_AS(breed_(get_index_(size_))) {
+		breed_(get_index_(size_));
+	}
+
+
+	private:
+	ProcessType breed_(LengthType index_) DD_NOEXCEPT_AS(
+		static_cast<PointerType>(m_allocator_.allocate(get_size_(index_)))
+	) {
+		PointerType temp_ = m_allocator_.allocate(get_size_(index_));
+		recycle_(temp_, index_);
+	}
+
+
+	public:
+	ProcessType clear() DD_NOEXCEPT {
+		for (LengthType current_ = 0; current_ < maximum_units; ++ current_) {
+			clear_(current_);
+		}
+	}
+
+	private:
+	ProcessType clear_(LengthType index_) DD_NOEXCEPT {
+		for (PointerType temp_ = m_free_lists_[index_]; temp_; m_free_lists_[index_] = temp_) {
+			temp_ = get_next_(temp_);
+			m_allocator_.deallocate(m_free_lists_[index_], get_size_(index_));
+		}
+	}
+
+
+	public:
+	PointerType allocate(SizeType size_) DD_NOEXCEPT_AS(fetch_(get_index_(size_))) {
+		if (size_ <= maximum_size) {
+			return fetch_(get_index_(size_));
+		} else {
+			return m_allocator_.allocate(size_);
+		}
+	}
+
+
+	public:
+	ProcessType deallocate(PointerType pointer_) DD_NOEXCEPT {
+		m_allocator_.deallocate(pointer_);
+	}
+
+	public:
+	ProcessType deallocate(PointerType pointer_, SizeType size_) DD_NOEXCEPT {
+		if (size_ <= maximum_size) {
+			recycle_(pointer_, get_index_(size_));
+		} else {
+			m_allocator_.deallocate(pointer_, size_);
+		}
+	}
+
+
+	public:
+#	if __cplusplus >= 201103L
+	template <typename ValueT__, typename... ArgumentsT__>
+	ProcessType construct(ValueT__* pointer_, ArgumentsT__&&... arguments___) noexcept(
+		noexcept(m_allocator_.construct(pointer_, forward<ArgumentsT__>(arguments___)...))
+	) {
+		m_allocator_.construct(pointer_, forward<ArgumentsT__>(arguments___)...);
+	}
+#	else
+	template <typename ValueT__>
+	ProcessType construct(ValueT__* pointer_, ValueT__ const& value___) {
+		m_allocator_.construct(pointer_, value__);
+	}
+#	endif
+
+
+	public:
+	template <typename ValueT__>
+	ProcessType destruct(ValueT__* pointer_) DD_NOEXCEPT {
+		m_allocator_.destruct(pointer_);
+	}
+
+
+	public:
+	template <typename ValueT__>
+	ProcessType destruct(ValueT__* begin_, ValueT__* end_) DD_NOEXCEPT {
+		m_allocator_.destruct(begin_, end_);
 	}
 
 
@@ -226,14 +443,14 @@ class Pool {
 #	if __cplusplus >= 201103L
 	template <typename ValueT__, typename... ArgumentsT__>
 	static ProcessType construct(ValueT__* pointer_, ArgumentsT__&&... arguments___) noexcept(
-		noexcept(BasicAllocatorType::construct(pointer_, forward<ArgumentsT__>(arguments___)...))
+		noexcept(m_pool_.construct(pointer_, forward<ArgumentsT__>(arguments___)...))
 	) {
-		BasicAllocatorType::construct(pointer_, forward<ArgumentsT__>(arguments___)...);
+		m_pool_.construct(pointer_, forward<ArgumentsT__>(arguments___)...);
 	}
 #	else
 	template <typename ValueT__>
 	static ProcessType construct(ValueT__* pointer_, ValueT__ const& value___) {
-		BasicAllocatorType::construct(pointer_, value__);
+		m_pool_.construct(pointer_, value__);
 	}
 #	endif
 
@@ -241,14 +458,14 @@ class Pool {
 	public:
 	template <typename ValueT__>
 	static ProcessType destruct(ValueT__* pointer_) DD_NOEXCEPT {
-		BasicAllocatorType::destruct(pointer_);
+		m_pool_.destruct(pointer_);
 	}
 
 
 	public:
 	template <typename ValueT__>
 	static ProcessType destruct(ValueT__* begin_, ValueT__* end_) DD_NOEXCEPT {
-		BasicAllocatorType::destruct(begin_, end_);
+		m_pool_.destruct(begin_, end_);
 	}
 
 
