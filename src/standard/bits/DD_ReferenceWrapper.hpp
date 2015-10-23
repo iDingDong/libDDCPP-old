613fe5d61bf5195671584738a74f5d86c523ab3f
iDingDong <idingdong@mogara.org>
2015-10-23 16:17:11 +0800

    Implement DD::ReferenceWrapper

--- src/standard/bits/DD_ReferenceWrapper.hpp
+++ src/standard/bits/DD_ReferenceWrapper.hpp
@@ -4,21 +4,83 @@
 
 
 
-#	if __cplusplus < 201103L
-#		error ISO/IEC 14882:2011 or a later version support is required for 'DD::ReferenceWrapper'.
+#	include "DD_address_of.hpp"
 
+
+
+DD_DETAIL_BEGIN_
+template <typename ObjectT_>
+struct ReferenceWrapper {
+	public:
+	DD_ALIAS(ThisType, ReferenceWrapper<ObjectT_>);
+	DD_ALIAS(Type, ObjectT_);
+
+	public:
+	DD_VALUE_TYPE_NESTED(Type)
+
+
+	private:
+	PointerType m_pointer_;
+
+
+	public:
+	DD_DELETE_DEFAULT_CONSTRUCTOR(ReferenceWrapper)
+
+#	if __cplusplus >= 201103L
+	public:
+	constexpr ReferenceWrapper(ThisType const& origin_) = default;
+
+#	endif
+	public:
+	DD_DELETE_MOVE_CONSTRUCTOR(ReferenceWrapper);
+
+
+	public:
+	DD_CONSTEXPR ReferenceWrapper(Type& object_) DD_NOEXCEPT : m_pointer_(address_of(object_)) {
+	}
+
+#	if __cplusplus >= 201103L
+	public:
+	ReferenceWrapper(Type&& object_) = delete;
+
+#	endif
+
+	public:
+	PointerType DD_CONSTEXPR get_pointer() const DD_NOEXCEPT {
+		return m_pointer_;
+	}
+
+	public:
+	ReferenceType DD_CONSTEXPR get_reference() const DD_NOEXCEPT {
+		return *get_pointer();
+	}
+
+
+#	if __cplusplus >= 201103L
+	public:
+	ThisType& operator =(ThisType const& origin_) = default;
+
+	public:
+	ThisType& operator =(ThisType&& origin_) = default;
 
 
 #	endif
-#	include <functional>
+	public:
+	DD_CONSTEXPR operator ReferenceType() const DD_NOEXCEPT {
+		return get_reference();
+	}
 
-#	include "DD_global_definitions.hpp"
+
+};
+
+
+
+DD_DETAIL_END_
 
 
 
 DD_BEGIN_
-template <typename ObjectT_>
-using ReferenceWrapper = std::reference_wrapper<ObjectT_>;
+using detail_::ReferenceType;
 
 
 
