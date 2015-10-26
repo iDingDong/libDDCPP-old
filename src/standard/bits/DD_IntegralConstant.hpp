0d94243bdce7e387a2c6ec0e73a3e96c73973fed
iDingDong <idingdong@mogara.org>
2015-10-26 21:45:27 +0800

    Change some implementation namespaces

--- src/standard/bits/DD_IntegralConstant.hpp
+++ src/standard/bits/DD_IntegralConstant.hpp
@@ -8,7 +8,7 @@
 
 
 
-DD_BEGIN_
+DD_DETAIL_BEGIN_
 template <typename ValueT_, ValueT_ value_c_>
 struct IntegralConstant {
 	DD_ALIAS(ValueType, ValueT_);
@@ -66,12 +66,34 @@ struct DifferenceConstant : IntegralConstant<DifferenceType, difference_c_> {
 template <typename IntegralT_>
 struct StdIntegralConstant : IntegralConstant<typename IntegralT_::value_type, IntegralT_::value> {
 };
+
+
+
+template <typename IntegralT_>
+struct StdBoolConstant : BoolConstant<IntegralT_::value> {
+};
 #	endif
 
 
 
 DD_ALIAS(TrueType, BoolConstant<true>);
 DD_ALIAS(FalseType, BoolConstant<false>);
+
+
+
+DD_DETAIL_END_
+
+
+
+DD_BEGIN_
+using detail_::IntegralConstant;
+using detail_::BoolConstant;
+using detail_::SizeConstant;
+using detail_::DifferenceConstant;
+using detail_::StdIntegralConstant;
+using detail_::StdBoolConstant;
+using detail_::TrueType;
+using detail_::FalseType;
 
 
 
