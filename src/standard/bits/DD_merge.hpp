ed0dd2196df10b3b25cb9e576e3fd1b78925af8b
iDingDong <idingdong@mogara.org>
2015-11-23 21:52:27 +0800

    Add safe version of 'DD::merge' and changed range support for it

--- src/standard/bits/DD_merge.hpp
+++ src/standard/bits/DD_merge.hpp
@@ -58,23 +58,82 @@ UndirectionalIteratorT3_ merge(
 	return copy(begin_1__, end_1__, result_begin__);
 }
 
-template <typename UndirectionalRangeT1_, typename UndirectionalRangeT2_, typename UndirectionalIteratorT_>
-inline UndirectionalIteratorT_ merge(
-	UndirectionalRangeT1_& range_1__,
-	UndirectionalRangeT2_& range_2__,
-	UndirectionalIteratorT_ result_begin__
-) DD_NOEXCEPT_AS(merge(DD_SPLIT_RANGE(range_1__) DD_COMMA DD_SPLIT_RANGE(range_2__) DD_COMMA result_begin__)) {
-	return merge(DD_SPLIT_RANGE(range_1__) DD_COMMA DD_SPLIT_RANGE(range_2__) DD_COMMA result_begin__);
+template <typename UndirectionalIteratorT1_, typename UndirectionalIteratorT2_, typename UndirectionalIteratorT3_>
+UndirectionalIteratorT3_ merge(
+	UndirectionalIteratorT1_ begin_1__,
+	UndirectionalIteratorT1_ end_1__,
+	UndirectionalIteratorT2_ begin_2__,
+	UndirectionalIteratorT2_ end_2__,
+	UndirectionalIteratorT3_ result_begin__,
+	UndirectionalIteratorT3_ result_end__
+) DD_NOEXCEPT_AS(copy(begin_1__, end_1__, result_begin__) DD_COMMA copy(begin_2__, end_2__, result_begin__) DD_COMMA *begin_1__ < *begin_2__) {
+	for (; begin_2__ != end_2__; ++result_begin__) {
+		if (begin_1__ == end_1__) {
+			return copy(begin_2__, end_2__, result_begin__, result_end__);
+		}
+		if (result_begin__ == result_end__) {
+			return result_begin__;
+		}
+		if (*begin_1__ < *begin_2__) {
+			*result_begin__ = *begin_1__;
+			++begin_1__;
+		} else {
+			*result_begin__ = *begin_2__;
+			++begin_1__;
+		}
+	}
+	return copy(begin_1__, end_1__, result_begin__, result_end__);
 }
 
-template <typename UndirectionalRangeT1_, typename UndirectionalRangeT2_, typename UndirectionalIteratorT_, typename BinaryPredicateT_>
-inline UndirectionalIteratorT_ merge(
+template <typename UndirectionalIteratorT1_, typename UndirectionalIteratorT2_, typename UndirectionalIteratorT3_, typename BinaryPredicateT_>
+UndirectionalIteratorT3_ merge(
+	UndirectionalIteratorT1_ begin_1__,
+	UndirectionalIteratorT1_ end_1__,
+	UndirectionalIteratorT2_ begin_2__,
+	UndirectionalIteratorT2_ end_2__,
+	BinaryPredicateT_ less__,
+	UndirectionalIteratorT3_ result_begin__,
+	UndirectionalIteratorT3_ result_end__
+) DD_NOEXCEPT_AS(copy(begin_1__, end_1__, result_begin__) DD_COMMA copy(begin_2__, end_2__, result_begin__) DD_COMMA less__(*begin_1__, *begin_2__)) {
+	for (; begin_2__ != end_2__; ++result_begin__) {
+		if (begin_1__ == end_1__) {
+			return copy(begin_2__, end_2__, result_begin__, result_end__);
+		}
+		if (result_begin__ == result_end__) {
+			return result_begin__;
+		}
+		if (less__(*begin_1__, *begin_2__)) {
+			*result_begin__ = *begin_1__;
+			++begin_1__;
+		} else {
+			*result_begin__ = *begin_2__;
+			++begin_1__;
+		}
+	}
+	return copy(begin_1__, end_1__, result_begin__, result_end__);
+}
+
+template <typename UndirectionalRangeT1_, typename UndirectionalRangeT2_, typename UndirectionalRangeT3_>
+inline DD_MODIFY_TRAIT(Iterator, UndirectionalRangeT3_) merge(
 	UndirectionalRangeT1_& range_1__,
 	UndirectionalRangeT2_& range_2__,
-	UndirectionalIteratorT_ result_begin__,
-	BinaryPredicateT_ less__
-) DD_NOEXCEPT_AS(merge(DD_SPLIT_RANGE(range_1__) DD_COMMA DD_SPLIT_RANGE(range_2__) DD_COMMA result_begin__) DD_COMMA less__) {
-	return merge(DD_SPLIT_RANGE(range_1__) DD_COMMA DD_SPLIT_RANGE(range_2__) DD_COMMA result_begin__, less__);
+	UndirectionalRangeT3_& result_range__
+) DD_NOEXCEPT_AS(static_cast<DD_MODIFY_TRAIT(Iterator, UndirectionalRangeT3_)>(
+	merge(DD_SPLIT_RANGE(range_1__) DD_COMMA DD_SPLIT_RANGE(range_2__) DD_COMMA DD_SPLIT_RANGE(result_range__))
+)) {
+	return merge(DD_SPLIT_RANGE(range_1__), DD_SPLIT_RANGE(range_2__), DD_SPLIT_RANGE(result_range__));
+}
+
+template <typename UndirectionalRangeT1_, typename UndirectionalRangeT2_, typename UndirectionalRangeT3_, typename BinaryPredicateT_>
+inline DD_MODIFY_TRAIT(Iterator, UndirectionalRangeT3_) merge(
+	UndirectionalRangeT1_& range_1__,
+	UndirectionalRangeT2_& range_2__,
+	BinaryPredicateT_ less__,
+	UndirectionalRangeT3_& result_range__
+) DD_NOEXCEPT_AS(static_cast<DD_MODIFY_TRAIT(Iterator, UndirectionalRangeT3_)>(
+	merge(DD_SPLIT_RANGE(range_1__) DD_COMMA DD_SPLIT_RANGE(range_2__) DD_COMMA less__ DD_COMMA DD_SPLIT_RANGE(result_range__))
+)) {
+	return merge(DD_SPLIT_RANGE(range_1__), DD_SPLIT_RANGE(range_2__), less__, DD_SPLIT_RANGE(result_range__));
 }
 
 
