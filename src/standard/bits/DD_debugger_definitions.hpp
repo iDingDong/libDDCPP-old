47afcadeac53870c4f982c7e545f046f76109d76
iDingDong <idingdong@mogara.org>
2015-12-11 20:57:20 +0800

    Enhance compatibility

--- src/standard/bits/DD_debugger_definitions.hpp
+++ src/standard/bits/DD_debugger_definitions.hpp
@@ -22,19 +22,11 @@
 
 
 
-#	if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__) || (defined(__DMC__) && (__DMC__ >= 0x810))
-#		define DD_CURRENT_FUNCTION __PRETTY_FUNCTION__
-#	elif defined(__FUNCSIG__)
-#		define DD_CURRENT_FUNCTION __FUNCSIG__
-#	elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
-#		define DD_CURRENT_FUNCTION __FUNCTION__
-#	elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
-#		define DD_CURRENT_FUNCTION __FUNC__
-#	elif (defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)) || (defined(__cplusplus) && (__cplusplus >= 201103))
+#	if __cplusplus >= 201103L
 #		define DD_CURRENT_FUNCTION __func__
 #	else
 #		define DD_CURRENT_FUNCTION "(Unknown)"
-#	endif// Support from boost
+#	endif
 #	define DD_CURRENT_FILE __FILE__
 #	define DD_CURRENT_LINE __LINE__
 
