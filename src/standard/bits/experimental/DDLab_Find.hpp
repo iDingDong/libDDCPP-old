4c637a9ab7794e329d995ba12eb48f827b3e7f76
iDingDong <idingdong@mogara.org>
2016-01-14 01:42:46 +0800

    Some preparations

---
+++ src/standard/bits/experimental/DDLab_Find.hpp
@@ -0,0 +1,33 @@
+//	DDCPP/standard/bits/experimental/DDLab_Find.hpp
+#ifndef DDLAB_FIND_HPP_INCLUDED_
+#	define DDLAB_FIND_HPP_INCLUDED_ 1
+
+
+
+#	include "DD_experimental_definitions.hpp"
+#	include "../DD_find.hpp"
+
+
+
+DD_LAB_DETAIL_BEGIN_
+struct Find {
+} DD_CONSTANT find;
+
+
+
+DD_LAB_DETAIL_END_
+
+
+
+DD_LAB_BEGIN_
+using detail_::Find;
+
+using detail_::find;
+
+
+
+DD_LAB_END_
+
+
+
+#endif
---
+++ src/standard/bits/experimental/DDLab_experimental_definitions.hpp
@@ -0,0 +1,18 @@
+//	DDCPP/standard/bits/experimental/DDLab_experimental_definitions.hpp
+#ifndef DDLAB_EXPERIMENTAL_DEFINITIONS_HPP_INCLUDED_
+#	define DDLAB_EXPERIMENTAL_DEFINITIONS_HPP_INCLUDED_ 1
+
+
+
+#	include "../DD_global_definitions.hpp"
+
+
+
+#	define DD_LAB_BEGIN_ DD_BEGIN_ namespace lab {
+#	define DD_LAB_END_ } DD_END_
+#	define DD_LAB_DETAIL_BEGIN_ DD_LAB_BEGIN_ namespace detail_ {
+#	define DD_LAB_DETAIL_END_ } DD_LAB_END_
+
+
+
+#endif
